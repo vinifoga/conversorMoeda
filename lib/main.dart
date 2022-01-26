b964ab4b008cd3b79e9f12a17c5eb283606911f3
Vinicius Fogaca <viniciusfernandes38@gmail.com>
2022-01-26 06:19:39 -0300

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("\$ Conversor \$"),),
+      appBar: AppBar(title: Text("\$ Conversor  \$"),),
     );
   }
 }

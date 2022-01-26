478e90587605c06be6a995a3d95d8eb61402daaf
Vinicius Fogaca <viniciusfernandes38@gmail.com>
2022-01-26 06:33:39 -0300

    Testing uri request

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 
-void main(){
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+Uri request = Uri.parse("https://api.hgbrasil.com/finance?format=json-cors&key=07456b3b");
+
+void main() async {
+
+  http.Response response = await http.get(request);
+  print(json.decode(response.body)["results"]["currencies"]["USD"]);
+
   runApp(const MaterialApp(
     home: Home(),
   ));
@@ -18,7 +26,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("\$ Conversor  \$"),),
+      appBar: AppBar(title: Text("\$ Conversor \$"),),
     );
   }
 }
